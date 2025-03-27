06b6aca960a0afc6f01dc1c79c578e017ebd6c04
RomanDuvakin <duvakin.r@GMAIL.COM>
2025-03-27 15:43:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Sisesta arv: ");
+        int arv = int.Parse(Console.ReadLine());
+
+        if (arv > 0)
+        {
+            Console.WriteLine("Arv on positiivne.");
+        }
+        else if (arv < 0)
+        {
+            Console.WriteLine("Arv on negatiivne.");
+        }
+        else
+        {
+            Console.WriteLine("Arv on null.");
+        }
+    }
+}
